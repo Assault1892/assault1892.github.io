b9dbc900923eef767971eb012e55528e34f27e37
Assault189 <vvvvvgaga12356@yahoo.co.jp>
2026-04-01 04:46:48 +0900

    fuwrai again

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,39 +1,102 @@
-import type I18nKeys from "../locales/keys";
+import type { AUTO_MODE, DARK_MODE, LIGHT_MODE } from "@constants/constants";
 
-interface Configuration {
-  title: string;
-  subTitle: string;
-  brandTitle: string;
+export type SiteConfig = {
+	title: string;
+	subtitle: string;
 
-  description: string;
+	lang:
+		| "en"
+		| "zh_CN"
+		| "zh_TW"
+		| "ja"
+		| "ko"
+		| "es"
+		| "th"
+		| "vi"
+		| "tr"
+		| "id";
 
-  site: string;
+	themeColor: {
+		hue: number;
+		fixed: boolean;
+	};
+	banner: {
+		enable: boolean;
+		src: string;
+		position?: "top" | "center" | "bottom";
+		credit: {
+			enable: boolean;
+			text: string;
+			url?: string;
+		};
+	};
+	toc: {
+		enable: boolean;
+		depth: 1 | 2 | 3;
+	};
 
-  locale: "ja-JP" | "en";
+	favicon: Favicon[];
+};
 
-  navigators: { nameKey: I18nKeys; href: string }[];
+export type Favicon = {
+	src: string;
+	theme?: "light" | "dark";
+	sizes?: string;
+};
 
-  username: string;
-  sign: string;
-  avatarUrl: string;
-
-  socialLinks: { icon: string; link: string }[];
-
-  maxSidebarCategoryChip: number;
-  maxSidebarTagChip: number;
-  maxFooterCategoryChip: number;
-  maxFooterTagChip: number;
-
-  banners: string[];
-
-  slugMode: "HASH" | "RAW";
-
-  license: {
-    name: string;
-    url: string;
-  };
-
-  bannerStyle: "LOOP";
+export enum LinkPreset {
+	Home = 0,
+	Archive = 1,
+	About = 2,
 }
 
-export type { Configuration };
+export type NavBarLink = {
+	name: string;
+	url: string;
+	external?: boolean;
+};
+
+export type NavBarConfig = {
+	links: (NavBarLink | LinkPreset)[];
+};
+
+export type ProfileConfig = {
+	avatar?: string;
+	name: string;
+	bio?: string;
+	links: {
+		name: string;
+		url: string;
+		icon: string;
+	}[];
+};
+
+export type LicenseConfig = {
+	enable: boolean;
+	name: string;
+	url: string;
+};
+
+export type LIGHT_DARK_MODE =
+	| typeof LIGHT_MODE
+	| typeof DARK_MODE
+	| typeof AUTO_MODE;
+
+export type BlogPostData = {
+	body: string;
+	title: string;
+	published: Date;
+	description: string;
+	tags: string[];
+	draft?: boolean;
+	image?: string;
+	category?: string;
+	prevTitle?: string;
+	prevSlug?: string;
+	nextTitle?: string;
+	nextSlug?: string;
+};
+
+export type ExpressiveCodeConfig = {
+	theme: string;
+};
